274d11bb804ffcdac952efdce5178c4132e5a829
Yohan3239 <66236743+Yohan3239@users.noreply.github.com>
2023-09-19 09:39:49 +0100

    Add project files.

---
+++ SelectionQuestions/Program.cs
@@ -0,0 +1,45 @@
+﻿class TestClass
+{
+    static string FizzBuzz(int num)
+    {
+        bool fizz = false;
+        bool buzz = false;
+        if (num % 3 == 0)
+        {
+            fizz = true;
+        }
+        if (num % 5 == 0)
+        {
+            buzz = true;
+        }
+        if (fizz && buzz)
+        {
+
+            return "FizzBuzz";
+
+        }
+        else if (fizz)
+        {
+
+            return "Fizz";
+
+        }
+        else if (buzz)
+        {
+
+            return "Buzz";
+
+        }
+        else
+        {
+            return num.ToString();
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        
+        FizzBuzz(15);
+    }
+}
+
